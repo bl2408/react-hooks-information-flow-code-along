f558dcb843c91f4814439d535462dfa3263b48a5
Brian Lambert <brianlambert2408@gmail.com>
2022-10-12 21:01:28 +1100

    Completed tasks

--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,21 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childCol, setChildColor] = useState("#FFF");
+
+  const handleColorChange =()=>{
+    setColor(getRandomColor());
+    handleChildColorChange(getRandomColor());
+  };
+
+  const handleChildColorChange =(col)=>{
+    setChildColor(col);
+  };
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor ={handleColorChange} color={childCol} />
+      <Child onChangeColor ={handleColorChange} color={childCol}/>
     </div>
   );
 }
